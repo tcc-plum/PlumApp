616063571a56d172cba1dd07540f81cc48279863
andferreiral <andrey123.ferreira@hotmail.com>
2019-08-24 12:47:40 -0300

    Alteração da porta de 5555 para 5000 e atualização da versão do Flask por conta de vulnerabilidade

--- runserver.py
+++ runserver.py
@@ -8,7 +8,7 @@ from PlumApp import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5000'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
